b2dc088618ed23a9b144689814d2d126d121e643
zeleney30 <michaelzeleney@gmail.com>
2021-12-14 13:30:43 -0500

    Session

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const session = require("express-session");
 const passport = require("passport");
 require("./Authentication");
 
-
-function isLoggedIn(req, res, next)
-{
-    req.user ? next() : res.sendStatus(401);
-}
-
-
 const app = express();
 
 app.use(express.json());
@@ -17,6 +10,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static(__dirname + "/public"));
+
+function isLoggedIn(req, res, next) {
+    // Logic: If the req object already has user credentaial, then pass it onto the next point, else return a 401 status (unauthorized acccess)
+    req.user ? next() : res.sendStatus(401);
+}
+
+app.use(session({ secret: "j6hs4w(*^Gw3bg5(*G^" })); // use a more complicated secret and save it in the .env file.
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('', (req, res) => {
   res.render('index');
