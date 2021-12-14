e4ca20ba7c7cbdbfe0f95c55886a6a32ef70a844
zeleney30 <michaelzeleney@gmail.com>
2021-12-14 13:59:04 -0500

    Fix Buttons

--- public/JS/Index.js
+++ public/JS/Index.js
@@ -1,18 +1,9 @@
-const express = require('express');
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-
-
-app.get('/', (req, res) =>
+function scrollDown()
 {
-    res.render("index", { name: "Michael"});
-})
+    document.getElementById('sectionMain').scrollIntoView();
+}
 
-app.listen(3000, () => 
+function goToPurchase()
 {
-    console.log("Server is running!");
-})
+    window.location.href = '/Purchase';
+}
