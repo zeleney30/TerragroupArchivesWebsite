e6a11f64cdb0a55d906d2ba0166e56b18e17c4ae
zeleney30 <michaelzeleney@gmail.com>
2021-12-14 13:44:27 -0500

    Remove excess unworking code

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require("express");
-const session = require("express-session");
 const passport = require("passport");
-require("./Authentication");
 
 const app = express();
 
@@ -10,15 +8,6 @@ app.use(express.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static(__dirname + "/public"));
-
-function isLoggedIn(req, res, next) {
-    // Logic: If the req object already has user credentaial, then pass it onto the next point, else return a 401 status (unauthorized acccess)
-    req.user ? next() : res.sendStatus(401);
-}
-
-app.use(session({ secret: "j6hs4w(*^Gw3bg5(*G^" })); // use a more complicated secret and save it in the .env file.
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.get('', (req, res) => {
   res.render('index');
@@ -31,7 +20,6 @@ app.get('/auth/google/callback', (req, res) => {
     res.render('index');
 })
 
-
 app.get('/index', (req, res) => {
     res.render('index');
 })
@@ -40,7 +28,7 @@ app.get('/ContactUs', (req, res) => {
     res.render('ContactUs');
 })
 
-app.get('/Purchase', isLoggedIn, (req, res) => {
+app.get('/Purchase', (req, res) => {
     res.render('Purchase');
 })
 
