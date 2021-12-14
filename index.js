ebd3922ec22fbb9ffa714548a768228aa1a4c208
zeleney30 <michaelzeleney@gmail.com>
2021-12-14 13:38:03 -0500

    Revert "Test"
    This reverts commit 24fbf0336fdcab0035e126e30b4b5c0a0b20ce1f.

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static(__dirname + "/public"));
 
 function isLoggedIn(req, res, next) {
+    // Logic: If the req object already has user credentaial, then pass it onto the next point, else return a 401 status (unauthorized acccess)
     req.user ? next() : res.sendStatus(401);
 }
 
-app.use(session({ secret: "j6hs4w(*^Gw3bg5(*G^" }));
+app.use(session({ secret: "j6hs4w(*^Gw3bg5(*G^" })); // use a more complicated secret and save it in the .env file.
 app.use(passport.initialize());
 app.use(passport.session());
-
-//  PAGES   //
 
 app.get('', (req, res) => {
   res.render('index');
 })
 
+// /auth/google
 app.get('/auth/google', passport.authenticate("google", { scope: ["email", "profile"] }));
 
 app.get('/auth/google/callback', (req, res) => {
@@ -41,8 +41,7 @@ app.get('/ContactUs', (req, res) => {
 })
 
 app.get('/Purchase', isLoggedIn, (req, res) => {
-    //res.render('Purchase');
-    res.send(`Hello.... ${req.user.displayName}`);
+    res.render('Purchase');
 })
 
 const PORT = process.env.PORT || 3000;
